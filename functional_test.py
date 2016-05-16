b6890282901c3fcb1814ad42cefd61238afbfa6a
Kyel John M. David <kyeljohnmdavid@gmail.com>
2016-05-16 21:44:26 +0800

    Fixed User stories

--- functional_test.py
+++ functional_test.py
@@ -20,7 +20,18 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertIn('To-Do',self.browser.title)
 		self.fail('Finish the Test')
 
+		# She is invited to enter a to-do item straight away
+		
+		# She types "Buy peacock feathers" into a text box (Edith's hobby
+		# is tying fly-fishing lures)
 
+		# When she hits enter, the page updates, and now the page lists
+		# "1: Buy peacock feathers" as an item in a to-do list
+
+		# There is still a text box inviting her to add another item. She
+		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+		# The page updates again, and now shows both items on her list
 
 
 if __name__ == '__main__':
