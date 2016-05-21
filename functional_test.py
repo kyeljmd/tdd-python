96080dc41b5ef85d286b2c70865e88fe7c6599c7
Kyel John M. David <kyeljohnmdavid@gmail.com>
2016-05-21 17:22:05 +0800

    Written Functional tests to support use case behaviors

--- functional_test.py
+++ functional_test.py
@@ -33,15 +33,20 @@ class NewVisitorTest(unittest.TestCase):
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
-
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-
-		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
-			'New To-do item did not appear in the table')	
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to fly')
+		inputbox.send_keys(Keys.ENTER)
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn('2: Use peacock feathers to fly', [row.text for row in rows])
+
+
+
 		self.fail('Finish The Test')
 		# The page updates again, and now shows both items on her list
 
